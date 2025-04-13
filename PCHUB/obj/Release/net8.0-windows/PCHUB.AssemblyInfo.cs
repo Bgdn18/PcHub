0e7fea6feed0a6e0a005fa840ce415048a15eb23
Bogdan <krati18012012@gmail.com>
2025-04-13 01:15:45 -0700

    first commit main

--- PCHUB/obj/Release/net8.0-windows/PCHUB.AssemblyInfo.cs
+++ PCHUB/obj/Release/net8.0-windows/PCHUB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f566af442ae61e274907defb0000794f5bf01105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c9c55bbc70853974334be6cce0740e1481da20")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
