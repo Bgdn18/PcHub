4ae9ec773290625273c933e91349924b5ad5a088
Bogdan <krati18012012@gmail.com>
2025-04-23 01:40:43 -0700

    update

--- PCHUB/obj/Debug/net8.0-windows/PCHUB.AssemblyInfo.cs
+++ PCHUB/obj/Debug/net8.0-windows/PCHUB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399cb20a8795e11a97630c16d8d4f0bd297db8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f967893b4e06d4bb7d44ec3938b31c706c012206")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCHUB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PCHUBBlockingApps/obj/Debug/net8.0/PCHUBBlockingApps.AssemblyInfo.cs
+++ PCHUBBlockingApps/obj/Debug/net8.0/PCHUBBlockingApps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCHUBBlockingApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399cb20a8795e11a97630c16d8d4f0bd297db8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0eb0233896c83f49f679fec59fc102ffa47d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCHUBBlockingApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCHUBBlockingApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
